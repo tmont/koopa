2d880a16a0de43a2d772330d7c7c3ca6fd9d3cf3
tmont <tommy.mont@gmail.com>
2012-08-19 18:13:47 -0700

    refactored mobile tests

--- tests/MobileTests.js
+++ tests/MobileTests.js
@@ -1,86 +1,68 @@
 (function(){
-
-	var koopaDeref = typeof(module) === 'undefined' ? window.koopa : require('../src/koopa');
+	var expect = typeof(module) === 'undefined' ? window.expect : require('expect.js');
 	var data = typeof(module) === 'undefined' ? window.koopaData : require('./data');
+	var koopaDeref = typeof(module) === 'undefined' ? window.koopa : require('../src/koopa');
 
-	var suite = Jarvis.suite('Mobile tests', [
-		function Should_detect_ipad() {
+	describe('Mobile tests', function() {
+		it('Should detect iPad', function() {
 			var info = koopaDeref(data.mobile.ipad);
+			expect(info).to.have.property('mobile', true);
+			expect(info).to.have.property('ipad', true);
+			expect(info).to.have.property('ios', true);
+		});
 
-			data.util.assertKoopaProperty(info, 'mobile');
-			data.util.assertKoopaProperty(info, 'ipad');
-			data.util.assertKoopaProperty(info, 'ios');
-		},
-
-		function Should_detect_ipod() {
+		it('Should detect iPod', function() {
 			var info = koopaDeref(data.mobile.ipod);
+			expect(info).to.have.property('mobile', true);
+			expect(info).to.have.property('ipod', true);
+			expect(info).to.have.property('ios', true);
+		});
 
-			data.util.assertKoopaProperty(info, 'mobile');
-			data.util.assertKoopaProperty(info, 'ipod');
-			data.util.assertKoopaProperty(info, 'ios');
-		},
-
-		function Should_detect_iphone() {
+		it('Should detect iPhone', function() {
 			var info = koopaDeref(data.mobile.iphone);
+			expect(info).to.have.property('mobile', true);
+			expect(info).to.have.property('iphone', true);
+			expect(info).to.have.property('ios', true);
+		});
 
-			data.util.assertKoopaProperty(info, 'mobile');
-			data.util.assertKoopaProperty(info, 'iphone');
-			data.util.assertKoopaProperty(info, 'ios');
-		},
-
-		function Should_detect_blackberry() {
+		it('Should detect Blackberry', function() {
 			var info = koopaDeref(data.mobile.blackberry);
+			expect(info).to.have.property('mobile', true);
+			expect(info).to.have.property('blackberry', true);
+		});
 
-			data.util.assertKoopaProperty(info, 'mobile');
-			data.util.assertKoopaProperty(info, 'blackberry');
-		},
-
-		function Should_detect_android() {
-			var info = koopaDeref(data.mobile.android);
-
-			data.util.assertKoopaProperty(info, 'mobile');
-			data.util.assertKoopaProperty(info, 'android');
-		},
-
-		function Should_detect_web_os() {
+		it('Should detect WebOS', function() {
 			var info = koopaDeref(data.mobile.webOs);
+			expect(info).to.have.property('mobile', true);
+			expect(info).to.have.property('webos', true);
+		});
 
-			data.util.assertKoopaProperty(info, 'mobile');
-			data.util.assertKoopaProperty(info, 'webos');
-		},
+		it('Should detect Android', function() {
+			var info = koopaDeref(data.mobile.android);
+			expect(info).to.have.property('mobile', true);
+			expect(info).to.have.property('android', true);
+		});
 
-		function Should_detect_windows_phone() {
+		it('Should detect Windows Phone', function() {
 			var info = koopaDeref(data.mobile.windowsPhone);
+			expect(info).to.have.property('mobile', true);
+			expect(info).to.have.property('windowsPhone', true);
+		});
 
-			data.util.assertKoopaProperty(info, 'mobile');
-			data.util.assertKoopaProperty(info, 'windowsPhone');
-		},
-
-		function Should_detect_opera_mini() {
+		it('Should detect Opera Mini', function() {
 			var info = koopaDeref(data.mobile.operaMini);
+			expect(info).to.have.property('mobile', true);
+			expect(info).to.have.property('operaMini', true);
+		});
 
-			data.util.assertKoopaProperty(info, 'mobile');
-			data.util.assertKoopaProperty(info, 'operaMini');
-		},
-
-		function Should_detect_ie_mobile() {
+		it('Should detect IE mobile', function() {
 			var info = koopaDeref(data.mobile.ieMobile);
-
-			data.util.assertKoopaProperty(info, 'mobile');
-			data.util.assertKoopaProperty(info, 'iemobile');
+			expect(info).to.have.property('mobile', true);
+			expect(info).to.have.property('iemobile', true);
 
 			info = koopaDeref(data.mobile.msieMobile);
-
-			data.util.assertKoopaProperty(info, 'mobile');
-			data.util.assertKoopaProperty(info, 'iemobile');
-		},
-	]);
-
-
-	if (typeof(module) === 'undefined') {
-		Jarvis.run(suite);
-	} else {
-		module.exports = suite;
-	}
-
+			expect(info).to.have.property('mobile', true);
+			expect(info).to.have.property('iemobile', true);
+		});
+	});
 }());
